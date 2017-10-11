500574df6e7cb5f26d698b79a3b5322ece4a88e4
Sean Moss <moss.seank@gmail.com>
2017-10-10 21:17:14 -0600

    SimManager now opens a blank Eto.Forms window

---
+++ SimManager/MainWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace NGSim
+{
+	public class MainWindow : Form
+	{
+		public MainWindow()
+		{
+			ClientSize = new Size(400, 300);
+			Title = "SimManager";
+		}
+	}
+}
--- SimManager/Program.cs
+++ SimManager/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using Eto.Forms;
 
 namespace NGSim
 {
 	public static class Program
 	{
+		[STAThread]
 		public static void Main(string[] args)
 		{
-
+			Application app = new Application();
+			app.Run(new MainWindow());
 		}
 	}
 }

5fda0bf465b71009c4fa66ad774476156ce8e7fd
Booker <boli5799@colorado.edu>
2017-10-24 18:25:59 -0600

    Attempted lidgren messaging.

--- SimManager/MainWindow.cs
+++ SimManager/MainWindow.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using Eto.Forms;
 using Eto.Drawing;
+using Lidgren.Network;
+using System.Net;
+using System.Net.Sockets;
 
 namespace NGSim
 {
 	public class MainWindow : Form
 	{
-		public MainWindow()
-		{
-			ClientSize = new Size(400, 300);
-			Title = "SimManager";
-		}
-	}
+        bool doOnce = false;
+        public MainWindow()
+        {
+            ClientSize = new Size(400, 300);
+            Title = "SimManager";
+            if (!doOnce)
+            {
+                doOnce = true;
+                SetServer();
+            }
+        }
+        public void SetServer() { 
+            NetPeerConfiguration config = new NetPeerConfiguration("test");
+            config.Port = 5432;
+            config.LocalAddress = new System.Net.IPAddress(new byte[] { 127, 0, 0, 1 });
+            config.MaximumConnections = 1000;
+            NetServer server = new NetServer(config);
+            server.Start();
+            NetIncomingMessage msg = null;
+            while (true)
+            {
+                while ((msg = server.ReadMessage()) != null)
+                {
+
+                    Console.WriteLine(msg.MessageType.ToString());
+                    /*string str = msg.ReadString();
+                    Int16 int16 = msg.ReadInt16();
+                    Console.WriteLine(str);
+                    Console.WriteLine("Ints: {0}", int16);*/
+                    if (msg.MessageType == NetIncomingMessageType.Data)
+                    {
+                        Console.WriteLine(msg.ReadInt16());
+                        Console.WriteLine(msg.ReadString());
+                    }
+                }
+            }
+        }
+    }
 }
--- SimViewer/SimViewer.cs
+++ SimViewer/SimViewer.cs
@@ -1,16 +1,39 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Lidgren.Network;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace NGSim
 {
 	public class SimViewer : Game
 	{
 		private GraphicsDeviceManager _graphics;
-
-		public SimViewer() :
-			base()
-		{
-			_graphics = new GraphicsDeviceManager(this);
-		}
-	}
+        bool doOnce = false;
+        public SimViewer() : base() {
+            _graphics = new GraphicsDeviceManager(this);
+            if (!doOnce)
+            {
+                doOnce = true;
+                SetClient();
+            }
+        }
+        public void SetClient() { 
+            NetPeerConfiguration config = new NetPeerConfiguration("test");
+            NetClient client = new NetClient(config);
+            client.Start();
+            client.Connect("127.0.0.1", 5432);
+            /*NetOutgoingMessage msg = client.CreateMessage();
+            msg.Write((Int16)3346);
+            msg.Write("Test Message 1 whooahaa");
+            client.SendMessage(msg, NetDeliveryMethod.ReliableUnordered);
+            client.FlushSendQueue();
+            NetOutgoingMessage msg2 = client.CreateMessage();
+            msg2.Write((Int16)48);
+            msg2.Write("deaxjnasxnajs");
+            client.SendMessage(msg2, NetDeliveryMethod.ReliableUnordered);
+            client.FlushSendQueue();*/
+        }
+    }
 }

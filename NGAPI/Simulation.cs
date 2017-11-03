d67edd573c7a3d8ae762de0c7f9a2e22162466ad
Austin Pearman <aupe5089@colorado.edu>
2017-11-02 18:10:13 -0600

    Added skeleton for Sim Class, pending review

---
+++ NGAPI/Simulation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NGApi
+{
+    class Simulation
+    {
+        private static int uavx;
+        private static int uavy;
+        private static int tankx;
+        private static int tanky;
+        private static bool isDetected;
+
+        //Entity Classes Will Exist here once built
+    }
+}

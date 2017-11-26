0a009245506cca6423f55cc15e3aecf80508b85f
Sean Moss <moss.seank@gmail.com>
2017-11-26 15:44:12 -0700

    Initial class for the world model, but now the tank and uav crash...

---
+++ SimViewer/Graphics/WorldModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NGSim.Graphics
+{
+	public class WorldModel : IDisposable
+	{
+		private VertexBuffer _vBuffer;
+		private IndexBuffer _iBuffer;
+		private BasicEffect _groundEffect;
+
+		public readonly int Size;
+
+		public WorldModel(GraphicsDevice device, int worldsize)
+		{
+			Size = worldsize;
+			int won2 = worldsize / 2;
+			VertexPositionColor[] vGround = new VertexPositionColor[4]
+			{
+				new VertexPositionColor(new Vector3(-won2, 0, -won2), Color.Green),
+				new VertexPositionColor(new Vector3(won2, 0, -won2), Color.Green),
+				new VertexPositionColor(new Vector3(won2, 0, won2), Color.Green),
+				new VertexPositionColor(new Vector3(-won2, 0, won2), Color.Green)
+			};
+			_vBuffer = new VertexBuffer(device, VertexPositionColor.VertexDeclaration, 4, BufferUsage.None);
+			_vBuffer.SetData(vGround);
+			ushort[] iGround = new ushort[6] { 0, 1, 3, 1, 2, 3 };
+			_iBuffer = new IndexBuffer(device, IndexElementSize.SixteenBits, 6, BufferUsage.None);
+			_iBuffer.SetData(iGround);
+
+			_groundEffect = new BasicEffect(device);
+			_groundEffect.VertexColorEnabled = true;
+			_groundEffect.TextureEnabled = false;
+			_groundEffect.LightingEnabled = false;
+		}
+
+		public void Draw(GraphicsDevice device, Camera camera)
+		{
+			_groundEffect.World = Matrix.Identity;
+			_groundEffect.View = camera.ViewMatrix;
+			_groundEffect.Projection = camera.ProjectionMatrix;
+			device.SetVertexBuffer(_vBuffer);
+			device.Indices = _iBuffer;
+			_groundEffect.CurrentTechnique.Passes[0].Apply();
+			device.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 4);
+		}
+
+		public void Dispose()
+		{
+			_iBuffer.Dispose();
+			_vBuffer.Dispose();
+			_groundEffect.Dispose();
+		}
+	}
+}
--- SimViewer/SimViewer.cs
+++ SimViewer/SimViewer.cs
@@ -17,12 +17,7 @@ namespace NGSim
 
 		private CModel _uavModel;
 		private CModel _tankModel;
-
-		// Temporary ground model
-		private VertexBuffer _vBuffer;
-		private IndexBuffer _iBuffer;
-		private BasicEffect _groundEffect;
-		private readonly int WORLD_SIZE = 50;
+		private WorldModel _world;
 
 		public SimViewer() :
 			base()
@@ -46,26 +41,8 @@ namespace NGSim
 			// Initialize the custom input manager
 			InputManager.Initialize();
 
-			// Initialize the ground
-			int won2 = WORLD_SIZE / 2;
-			VertexPositionColor[] vGround = new VertexPositionColor[4]
-			{
-				new VertexPositionColor(new Vector3(-won2, 0, -won2), Color.Green),
-				new VertexPositionColor(new Vector3(won2, 0, -won2), Color.Green),
-				new VertexPositionColor(new Vector3(won2, 0, won2), Color.Green),
-				new VertexPositionColor(new Vector3(-won2, 0, won2), Color.Green)
-			};
-			_vBuffer = new VertexBuffer(GraphicsDevice, VertexPositionColor.VertexDeclaration, 4, BufferUsage.None);
-			_vBuffer.SetData(vGround);
-			ushort[] iGround = new ushort[6] { 0, 1, 3, 1, 2, 3 };
-			_iBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, 6, BufferUsage.None);
-			_iBuffer.SetData(iGround);
-
-			// Initialize the ground shader
-			_groundEffect = new BasicEffect(GraphicsDevice);
-			_groundEffect.VertexColorEnabled = true;
-			_groundEffect.TextureEnabled = false;
-			_groundEffect.LightingEnabled = false;
+			// Create the world
+			_world = new WorldModel(GraphicsDevice, 50);
 
 			// Create the camera
 			CameraManager.Set(new ArcBallCamera(GraphicsDevice, distance: 20f, yaw: 0f, pitch: 45f), new ArcBallCameraBehavior());
@@ -108,14 +85,7 @@ namespace NGSim
 
 			Camera camera = CameraManager.ActiveCamera;
 
-			_groundEffect.World = Matrix.Identity;
-			_groundEffect.View = camera.ViewMatrix;
-			_groundEffect.Projection = camera.ProjectionMatrix;
-			GraphicsDevice.SetVertexBuffer(_vBuffer);
-			GraphicsDevice.Indices = _iBuffer;
-			_groundEffect.CurrentTechnique.Passes[0].Apply();
-			GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 4);
-
+			_world.Draw(GraphicsDevice, camera);
 			//_tankModel.Render(camera, Vector3.Right * 3 + Vector3.Up * 2);
 			//_uavModel.Render(camera, Vector3.Left * 3 + Vector3.Up * 5);
 

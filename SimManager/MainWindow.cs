1b629c263a455ae3874078c6eda3c39abf8f8126
Dalton Morrow <dalton.morrow@colorado.edu>
2017-12-07 10:15:43 -0700

    Spaces -> Tabs

--- SimManager/MainWindow.cs
+++ SimManager/MainWindow.cs
@@ -7,45 +7,45 @@ namespace NGSim
 {
 	public class MainWindow : Form
 	{
-        private static Logger logger = LogManager.GetCurrentClassLogger();
-        private TextBox AlgorithmTextBox1;
-        private TextBox AlgorithmTextBox2;
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private TextBox AlgorithmTextBox1;
+		private TextBox AlgorithmTextBox2;
 
-        public MainWindow()
+		public MainWindow()
 		{
-            logger.Info("Sample Log Message");
+			logger.Info("Sample Log Message");
 			ClientSize = new Size(500, 100);
 			Title = "SimManager";
 
-            var layout = new TableLayout();
-            layout.Spacing = new Size(5, 5);
-            layout.Padding = new Padding(10, 10, 10, 10);
+			var layout = new TableLayout();
+			layout.Spacing = new Size(5, 5);
+			layout.Padding = new Padding(10, 10, 10, 10);
 
-            AlgorithmTextBox1 = new TextBox { Text = "Algorithm 1 Path" };
-            AlgorithmTextBox2 = new TextBox { Text = "Algorithm 2 Path" };
+			AlgorithmTextBox1 = new TextBox { Text = "Algorithm 1 Path" };
+			AlgorithmTextBox2 = new TextBox { Text = "Algorithm 2 Path" };
 
-            layout.Rows.Add(new TableRow(
-                new TableCell(AlgorithmTextBox1, true),
-                new TableCell(AlgorithmTextBox2, true)
-            ));
+			layout.Rows.Add(new TableRow(
+				new TableCell(AlgorithmTextBox1, true),
+				new TableCell(AlgorithmTextBox2, true)
+			));
 
-            var GoButton = new Button { Text = "GO" };
+			var GoButton = new Button { Text = "GO" };
 
-            GoButton.Click += GoButton_Click;
+			GoButton.Click += GoButton_Click;
 
-            var GoRow = new TableRow(
-                new TableCell(GoButton, true)
-            );
+			var GoRow = new TableRow(
+				new TableCell(GoButton, true)
+			);
 
-            layout.Rows.Add(GoRow);
+			layout.Rows.Add(GoRow);
 
-            this.Content = layout;
+			this.Content = layout;
 		}
-        private void GoButton_Click(object sender, EventArgs e)
-        {
-            var OutString = "You entered: " + AlgorithmTextBox1.Text + " and " + AlgorithmTextBox2.Text;
-            MessageBox.Show(Application.Instance.MainForm, OutString, "GO Button", MessageBoxButtons.OK);
-        }
+		private void GoButton_Click(object sender, EventArgs e)
+		{
+			var OutString = "You entered: " + AlgorithmTextBox1.Text + " and " + AlgorithmTextBox2.Text;
+			MessageBox.Show(Application.Instance.MainForm, OutString, "GO Button", MessageBoxButtons.OK);
+		}
 
-    }
+	}
 }

f5670b69eef8f1551495607ed68f7ad39bd8ba84
Dalton Morrow <dalton.morrow@colorado.edu>
2018-01-24 01:34:11 -0700

    Added File Browsers
    Two 'Browse' buttons which open a file browser dialogue that populate the text fields.

--- SimManager/MainWindow.cs
+++ SimManager/MainWindow.cs
@@ -10,11 +10,15 @@ namespace NGSim
 		private static Logger logger = LogManager.GetCurrentClassLogger();
 		private TextBox AlgorithmTextBox1;
 		private TextBox AlgorithmTextBox2;
+		private Button AlgorithmBrowseButton1;
+		private Button AlgorithmBrowseButton2;
+		private OpenFileDialog AlgorithmOpenFile1;
+		private OpenFileDialog AlgorithmOpenFile2;
 
 		public MainWindow()
 		{
 			logger.Info("Sample Log Message");
-			ClientSize = new Size(500, 100);
+			ClientSize = new Size(800, 100);
 			Title = "SimManager";
 
 			var layout = new TableLayout();
@@ -24,14 +28,26 @@ namespace NGSim
 			AlgorithmTextBox1 = new TextBox { Text = "Algorithm 1 Path" };
 			AlgorithmTextBox2 = new TextBox { Text = "Algorithm 2 Path" };
 
+			AlgorithmBrowseButton1 = new Button { Text = "Browse" };
+			AlgorithmBrowseButton2 = new Button { Text = "Browse" };
+
+			AlgorithmBrowseButton1.Click += AlgorithmBrowseButton1_Click;
+			AlgorithmBrowseButton2.Click += AlgorithmBrowseButton2_Click;
+
+			AlgorithmOpenFile1 = new OpenFileDialog();
+			AlgorithmOpenFile2 = new OpenFileDialog();
+
 			layout.Rows.Add(new TableRow(
 				new TableCell(AlgorithmTextBox1, true),
-				new TableCell(AlgorithmTextBox2, true)
+				new TableCell(AlgorithmBrowseButton1, false),
+				new TableCell(),
+				new TableCell(AlgorithmTextBox2, true),
+				new TableCell(AlgorithmBrowseButton2, false)
 			));
 
-			var Run1GameInDepthButton = new Button { Text = "Run 1 Game In Depth With Positions Shown" };
-			var RunGamesContinuallyButton = new Button { Text = "Continually run games and print who's the winner" };
-			var Run500GamesContinuallyButton = new Button { Text = "Continually run 500 games and print the winning percentages" };
+			var Run1GameInDepthButton = new Button { Text = "Single Game" }; //Run 1 Game In Depth With Positions Shown
+			var RunGamesContinuallyButton = new Button { Text = "Continuous Games" }; //Continually run games and print who's the winner
+			var Run500GamesContinuallyButton = new Button { Text = "500 Continuous Games" }; //Continually run 500 games and print the winning percentages
 
 			Run1GameInDepthButton.Click += Run1GameInDepthButton_Click;
 			RunGamesContinuallyButton.Click += RunGamesContinuallyButton_Click;
@@ -67,6 +83,17 @@ namespace NGSim
 		{
 			UpdateManager.SimManager.running = true;
 			UpdateManager.SimManager.SetGameRunningMode(2);
+		}
+
+		private void AlgorithmBrowseButton1_Click(object sender, EventArgs e)
+		{
+			DialogResult result = AlgorithmOpenFile1.ShowDialog(AlgorithmBrowseButton1);
+			AlgorithmTextBox1.Text = AlgorithmOpenFile1.FileName;
+		}
+		private void AlgorithmBrowseButton2_Click(object sender, EventArgs e)
+		{
+			DialogResult result = AlgorithmOpenFile2.ShowDialog(AlgorithmBrowseButton2);
+			AlgorithmTextBox2.Text = AlgorithmOpenFile2.FileName;
 		}
 	}
 
